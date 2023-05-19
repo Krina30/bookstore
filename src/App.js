3b8e4a4469767edf21327c8b1376503fa6e26343
krina@30 <21dce117@charusat.edu.in>
2023-05-19 13:06:51 +0530

    component

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {routes ,route ,BrowserRouter,Link} from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
+function App()
+ {
+  return (
+   <BrowserRouter>
+   <div>
+      <Link to="/">Register</Link>
+      <Link to="/login">Login</Link>
+   </div>
+   <routes>
+      <route exact path="/" element={<Register/>}/>
+      <route exact path="/login" element={<Login/>}/>
+   </routes>
+   </BrowserRouter>
+  );
+}
+export default App;
