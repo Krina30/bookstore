37dd14ac521f17ac046f68abd7f48bc5e3ef1a69
krina@30 <21dce117@charusat.edu.in>
2023-05-22 20:57:58 +0530

    change

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import {Routes ,Route ,BrowserRouter,Link} from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Errorfound from './pages/Errorfound';
+import logo from './logo.svg';
 function App()
  {
   return (
    <BrowserRouter>
-   <div>
+   <img src={logo}/>
+   <div style={{padding: 5,display: "flex", columnGap: 10}}>
       <Link to="/">Register</Link>
       <Link to="/login">Login</Link>
+      <Link to="/error">404 error</Link>
   </div>
    <Routes>
       <Route exact path="/" element={<Register/>}/>
       <Route exact path="/login" element={<Login/>}/>
+      <Route exact path='/error' element={<Errorfound/>}/>
   </Routes>
    </BrowserRouter>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,16 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 function Login()
 {
+        const Navigate = useNavigate();
+        const onRegister=() => {
+        //Navigate("/")
+        alert("the button is clicked")
+        };
     return(
         <div>
         <h1> Login works </h1>
+        <button onClick={onRegister}>go to register</button>
         </div>
     );
 }
